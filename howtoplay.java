3bb57c74d74b0dc40ded7b91bb46bcd01dcd7fb6
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-11-27 03:09:55 +0900

    배경 이미지 바꾸기 (버튼 새로 추가하기 전)

--- howtoplay.java
+++ howtoplay.java
@@ -1,5 +1,6 @@
 package mypart;
 
+import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -8,32 +9,44 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.event.*;
 
    class Ex1 extends JFrame{
       // 뒷 배경
-      Image backgroundImage = new ImageIcon("Image/start.jpg").getImage();
+      Image backgroundImage = new ImageIcon("Image/rule.png").getImage();
       // 버튼 이미지
-      ImageIcon startBtnImage = new ImageIcon("BtnImage/Btn_Play.png");
+      JButton bt;
       
      Ex1(){
            this.setTitle("이미지 그리기 연습");
+           this.setSize(1200,900);
            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           this.setLocationRelativeTo(null);
+         
            
+	    	
            MyPanel panel = new MyPanel();
            panel.setLayout(new FlowLayout());
            
-           JButton button = new JButton(startBtnImage);
-           panel.add(new JButton(startBtnImage));
+           
+       
+	    	 
+           bt = new JButton(new ImageIcon("BtnImage/CheckBtn.png"));
+	       bt.setBounds(970,700,200,150);
+           
 
            this.add(panel);
            this.setSize(1200,900);
            this.setVisible(true);
-           
-           button.setBorderPainted(false); // JButton의 Border(외곽선)을 없애준다
-           button.setContentAreaFilled(false); // JButton의 내용영역 채우기 안함
-           button.setFocusPainted(false); // JButton이 선택(focus)되었을 때 생기는 테두리 사용안함 
-           button.setOpaque(false);
+         
+	    	
+	    	 bt.setContentAreaFilled(false);
+	    	 bt.setFocusPainted(false);
+	    	// bt.setBorderPainted(false);
+
+		
+	    	 setVisible(true);
       
         
        }
