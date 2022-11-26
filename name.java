92c8b1be469f858c8a50142f36feeece666eeb09
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-11-26 16:30:36 +0900

    이름 입력창 넣기 (크기 수정 전)

--- name.java
+++ name.java
@@ -1,54 +1,65 @@
 package mypart;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-
-	public class name extends JFrame{
-		JFrame frame = new JFrame();
-					// 생성자는 new를 통해 class를 instance화 시킬 때 호출되는 함수
-					JLabel img = new JLabel();
-					BufferedImage picture;
-		
-
-					
-					public  name(){
-						this.frame.setVisible(true);
-						this.frame.setSize(1200, 900); 
-						this.setResizable(false); // 창의 크기를 변경하지 못하게
-						//this.frame.getContentPane().setBackground(new Color(79, 19, 19));
-						this.frame.setLayout(new BorderLayout());
-						this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // 닫기버튼을 눌렀을 때 해지하기
-						this.frame.setLocationRelativeTo(null); // 자동으로 가운데에서 출력
-						this.add(img);
-
-						try {
-							picture = ImageIO.read(new File("./okay1.png"));
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-
-					@Override
-
-					public void paint(Graphics g) {
-						img.setIcon(new ImageIcon(picture.getScaledInstance(img.getWidth(), img.getHeight(), 4)));
-						super.paint(g);
-					}
-
-
-					public static void main(String[] args) {
-						new name();
-						
-					}
-
-	}
+public class name extends JFrame{
+			  BufferedImage img = null;
+			  JTextField logintTextField;
+			  JButton bt;
+		     public static void main(String[] args) {
+		    	 new name();
+		    	 
+		     }
+		     
+		     public name() {
+		    	 setTitle("닉네입 입력");
+		    	 setSize(1200,900);
+		    	 setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		    	 setLayout(null);
+		    	 
+		    	 // 이미지 빌려오기
+		    	 try {
+		    		 img = ImageIO.read(new File("Image/okay1.png"));
+		    	 }catch(IOException e) {
+		    		 System.out.println("이미지 불러오기 실패");
+		    		 System.exit(0);
+		    	 }
+		    	 
+		    	 JLayeredPane layeredPane = new JLayeredPane();
+		    	 layeredPane.setBounds(0,0,1200,900);
+		    	 layeredPane.setLayout(null);
+		    	 
+		    	 MyPanel panel = new MyPanel();
+		    	 panel.setBounds(0, 0, 1200, 900);
+		    	
+		    	 logintTextField = new JTextField(5);
+		    	 logintTextField.setBounds(162,450,901,100); 
+		    	 layeredPane.add(logintTextField);
+		    	 
+		    	 // 로그인버튼 추가
+//		    	 bt = new JButton(new ImageIcon("BtnImage/뭐시기 일단"));
+//		    	 bt.setBounds(755,689,104,48);
+//		    	 layeredPane.add(bt);
+		    	 
+		    	 layeredPane.add(panel);
+		    	 
+		    	 add(layeredPane);
+		    	 setVisible(true);
+		     }
+		     class MyPanel extends JPanel{
+		    	 public void paint(Graphics g) {
+		    		 g.drawImage(img,  0, 0, null);
+		    	 }
+		     }
+		     
+}
