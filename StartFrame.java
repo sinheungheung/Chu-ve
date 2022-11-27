6049f7eec34f37a037a33bc28b0fa775c7ef7131
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-11-28 07:32:00 +0900

    배경 음악 넣기 완료

--- StartFrame.java
+++ StartFrame.java
@@ -11,27 +11,46 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import javax.swing.event.*;
 
+
+
+
+
 class Start extends JFrame {
 	
+	
+	
 	// 버튼 이미지
-	ImageIcon startBtnImage = new ImageIcon("src/image/pepper.png");
-	ImageIcon explainBtnImage = new ImageIcon("src/image/lettuce.png");
+	ImageIcon startBtnImage = new ImageIcon("Image/pepper.png");
+	ImageIcon explainBtnImage = new ImageIcon("Image/lettuce.png");
 	JButton startBtn = new JButton(startBtnImage);
 	JButton explainBtn = new JButton(explainBtnImage);
 	
+	
+ 
 	Start() {
+		
 		// 뒷 배경
 		JPanel panel = new JPanel() {
-			Image backgroundImage = new ImageIcon("src/image/startbackground.png").getImage();
+			
+			Image backgroundImage = new ImageIcon("Image/startbackground.png").getImage();
 			@Override
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
@@ -56,6 +75,7 @@ class Start extends JFrame {
 		panel.add(startBtn);
 		setSize(1200, 900);
 		add(panel);
+		
 		
 		Dimension frameSize = getSize();
 		 
@@ -86,10 +106,43 @@ class Start extends JFrame {
 		});
 		
 	}
+	
 }
+
+
 
 public class StartFrame {
 	public static void main(String[] args) {
+		playMusic();
 		new Start();
+		
+         
+            }
+
+	private static void playMusic() {
+		   File bgm;
+		      AudioInputStream stream;
+		      AudioFormat format;
+		      DataLine.Info info;
+
+		      bgm = new File("audio/backgroundMusic.wav");
+		      Clip clip;
+
+		      try {
+		         stream = AudioSystem.getAudioInputStream(bgm);
+		         format = stream.getFormat();
+		         info = new DataLine.Info(Clip.class, format);
+		         clip = (Clip) AudioSystem.getLine(info);
+		         clip.open(stream);
+		         clip.start();
+		      } catch (Exception e) {
+		         System.out.println("err" + e);
+		      }
 	}
 }
+
+
+
+
+	
+
